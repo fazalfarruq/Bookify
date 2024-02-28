4f32eb014bfa1465a3da9721ce161911099c5260
Fazal Farruq <fazalfarruq@gmail.com>
2024-02-28 08:29:52 -0500

    added application with Query and Command

---
+++ Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+internal sealed class GetBookingQueryHandler : IRequestHandler<GetBookingQuery, Result<BookingResponse>>
+{
+    public Task<Result<BookingResponse>> Handle(GetBookingQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
